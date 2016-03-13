8a5eb63795c6ec7ee9b05475193fcf123d9d495e
Thran <thran@centrum.cz>
2016-03-13 22:19:16 +0100

    migrate to newer django and add constance

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pip install -r $DIR/requirements.txt
 
 # database
-python $DIR/manage.py syncdb
 python $DIR/manage.py migrate
 
 # static files
--- faces/migrations/0001_initial.py
+++ faces/migrations/0001_initial.py
@@ -1,34 +1,53 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import faces.models
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Face'
-        db.create_table(u'faces_face', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('name', self.gf('django.db.models.fields.CharField')(max_length=100)),
-            ('photo', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
-        ))
-        db.send_create_signal(u'faces', ['Face'])
+    dependencies = [
+    ]
 
-
-    def backwards(self, orm):
-        # Deleting model 'Face'
-        db.delete_table(u'faces_face')
-
-
-    models = {
-        u'faces.face': {
-            'Meta': {'object_name': 'Face'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'photo': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['faces']
+    operations = [
+        migrations.CreateModel(
+            name='Chat',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.CharField(max_length=255)),
+                ('who', models.CharField(max_length=255)),
+                ('time', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Face',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=100, verbose_name='Jm\xe9no')),
+                ('alternatives', models.CharField(max_length=255, null=True, verbose_name='Alternativn\xed jm\xe9na, odd\u011blen\xe1 st\u0159edn\xedkem', blank=True)),
+                ('hint', models.CharField(max_length=255, null=True, verbose_name='N\xe1pov\u011bda', blank=True)),
+                ('photo', models.ImageField(upload_to=faces.models.update_filename)),
+                ('photo_thumb', models.CharField(max_length=255, null=True, blank=True)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Guess',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('text', models.CharField(max_length=255)),
+                ('who', models.CharField(max_length=255)),
+                ('correct', models.BooleanField()),
+                ('face', models.ForeignKey(to='faces.Face')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
