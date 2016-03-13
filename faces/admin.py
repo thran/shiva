153337d8b7604ec6578b0e1fc40a42399b994090
Thran <thran@centrum.cz>
2016-03-13 23:17:27 +0100

    add all models to admin

--- faces/admin.py
+++ faces/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from faces.models import Face
+from faces.models import Face, Guess, Chat
 
 
 class FacesAdmin(admin.ModelAdmin):
     list_display = ("pk", "name", "alternatives", "hint", "active")
 
+
+class GuessAdmin(admin.ModelAdmin):
+    list_display = ("face", "text", "who", "correct")
+
+
+class ChatAdmin(admin.ModelAdmin):
+    list_display = ("who", "text", "time")
+
 admin.site.register(Face, FacesAdmin)
+admin.site.register(Guess, GuessAdmin)
+admin.site.register(Chat, ChatAdmin)
